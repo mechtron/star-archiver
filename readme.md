f9ea1ca19351b4782a33bee916d34928a0321c12
Corey <mechtrondev@gmail.com>
2018-05-19 18:25:45 -0700

    Update version storage mechanism to remove need for seperate 'version' file & add validation for existance of git and zip commands

--- readme.md
+++ readme.md
@@ -4,7 +4,13 @@
 
 Given a GitHub username, this script will look up the user's starred respositories, clone each repository and (optionally) compress them into a single tarball (`{username}_starred_repos_{timestamp}.tar.gz`).
 
-## (optional) Dependencies
+## Dependencies
+
+### Required
+
+    git
+
+### Optional
 
     zip
 
@@ -16,18 +22,24 @@ Given a GitHub username, this script will look up the user's starred respositori
 
     stararchiver <github-username>
 
-## Compress repositories into a tarball
+### Compress repositories into a single zip file
 
     stararchiver <github-username> -c
 
-## Specify the output directory
+### Specify the output directory
 
     stararchiver <github-username> -o ~/starred_repos
 
-## (optional) Specify a GitHub user token
+### (optional) Specify a GitHub user token
 
 There is an optional parameter `-t`/`--token` that can be used to specify a GitHub user API token. Doing this will allow you to perform more successive API calls, allowing for the archive of longer GitHub user star lists.
 
-## Build a new pip package
+## pip packaging
+
+### Build a new pip package
 
     ./build.sh
+
+## Upload a new package to PyPi
+
+    ./update-pip.sh
--- setup.py
+++ setup.py
@@ -1,25 +1,40 @@
 import os
+import re
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 def get_version():
-    return open(os.path.join(os.path.dirname(__file__), 'version')).read()
+    rsa_source = open(
+        os.path.join(os.path.dirname(__file__), 'stararchiver')
+    ).read()
+    version = re.search(
+        r"^SA_VERSION = ['\"]([^'\"]*)['\"]",
+        rsa_source,
+        re.M,
+    ).group(1)
+    if not version:
+        raise RuntimeError('Cannot find stararchiver version')
+    return version
 
 
 setup(
     name='stararchiver',
+    python_requires='>=2.6, <3',
     version=get_version(),
     scripts=['stararchiver'],
+    packages=find_packages(),
+    install_requires=[
+        'GitPython>=2.1.9',
+        'requests>=2.18.4',
+    ],
     long_description=read('README.md'),
     license='MIT',
     keywords = "github stars archiver",
-    packages=find_packages(),
-    data_files=['version'],
-    include_package_data=True,
     classifiers=[
         #   3 - Alpha
         #   4 - Beta
@@ -27,7 +42,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ],
